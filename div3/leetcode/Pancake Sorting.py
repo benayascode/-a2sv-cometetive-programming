21ef4177b32a3a89c32565ca3522fa67fe4132bb
betelcode <167538440+benayascode@users.noreply.github.com>
2024-08-14 14:42:18 +0300

    Create Pancake Sorting.py

---
+++ div3/leetcode/Pancake Sorting.py
@@ -0,0 +1,21 @@
+class Solution:
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+        def flip(k):
+            s = 0
+            while s < k:
+                arr[s], arr[k] = arr[k], arr[s]
+                s += 1
+                k -= 1
+        
+        out = []
+        for i in range(len(arr)-1, 0, -1):
+            maxi = i
+            for j in range(i, -1, -1):
+                if arr[j] > arr[maxi]:
+                    maxi = j
+            if maxi != i:
+                flip(maxi)
+                flip(i)
+                out.append(maxi + 1)
+                out.append(i + 1)
+        return out
